458bb95699d10049102df3351798482864da4873
ICEPOR\skandula <sanjuk1218@gmail.com>
2018-03-13 12:20:15 +0530

    date picker integration

--- src/app/date-component/date-component.component.ts
+++ src/app/date-component/date-component.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-date-component',
   templateUrl: './date-component.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./date-component.component.css']
 })
 export class DateComponentComponent implements OnInit {
@@ -10,6 +11,10 @@ export class DateComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+     setTimeout(() => {
+      const $ = window["$"];
+      $('#datetimepicker').datepicker({});
+     }, 10);
   }
   gotoBackPage(){
     window.history.back();
